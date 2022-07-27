745a050302f869a0d361d81a17849bba2171eafb
danielom23-meet <109900040+danielom23-meet@users.noreply.github.com>
2022-07-27 16:48:13 +0300

    home route added

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -13,9 +13,22 @@ password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/login', methods = ['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+	  	user = request.form['username']
+	  	pas = request.form['password']
+	  	if user == username and pas == password:
+	  		redirect(url_for('home'))
+	  	else:
+	  			return render_template('login.html')
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
   
 
 
